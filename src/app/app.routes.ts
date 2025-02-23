881c0fa7badb3efdaf0caf979df7300c859bdefc
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-23 12:36:07 +0200

    complete routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    async loadComponent() {
+      const m = await import('#/app/home/home.component');
+      return m.HomeComponent;
+    },
+  },
+  {
+    path: "todo",
+    async loadComponent() {
+      const m = await import('#/app/todo/todo.component');
+      return m.TodoComponent;
+    }
+  }
+];
