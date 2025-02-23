622ffec14465f4ec738fc9a101cbff981b4bb698
Axole Maranjana <AxoleMaranjana4@gmail.com>
2025-02-23 13:50:05 +0200

    custom directive and click handler

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { HighlightCompletedTodoDirective } from '#/app/directives/highlight-completed-todo.directive';
+import { Todo } from '#/app/model/todo/index.type';
+import { Component, input, output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
-  imports: [],
+  imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
+  todo = input.required<Todo>();
+  todoToggled = output<Todo>()
 
+  todoClicked(){
+    this.todoToggled.emit(this.todo())
+  }
+
+  constructor() {}
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,18 +1,26 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodoService } from '../services/todo/todo.service';
 import { Todo } from '../model/todo/index.type';
-import { catchError } from 'rxjs';
-import { NgIf } from '@angular/common';
+import { catchError, map } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
 })
 export class TodoComponent implements OnInit {
   todoService = inject(TodoService);
   todoItems = signal<Array<Todo>>([]);
+
+  updateTodoState(todo: Todo) {
+    this.todoItems.update((todos) =>
+      todos.map((todo_) =>
+        todo.id === todo_.id ? { ...todo_, completed: !todo.completed } : todo_
+      )
+    );
+  }
 
   // Runs on first mount only
   ngOnInit(): void {
